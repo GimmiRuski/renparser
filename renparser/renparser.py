abbb84ae595f719ab9b0ba45d5abf37d0bef6757
GimmiRuski <gimmiruski+github@gmail.com>
2021-01-11 20:08:49 +0100

    Add profile flag

--- renparser/renparser.py
+++ renparser/renparser.py
@@ -1,3 +1,5 @@
+import time
+
 import click
 from lark.indenter import Indenter
 from lark.lark import Lark
@@ -17,14 +19,22 @@ class RenpyIndenter(Indenter):
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
+@click.option("-p", "--profile", is_flag=True)
 @click.argument("script", type=click.File())
-def main(script):
+def main(script, profile):
     script_content = script.read()
     parser = Lark.open(
         "renpy.lark", parser="lalr", postlex=RenpyIndenter(), rel_to=__file__
     )
+    if profile:
+        start_time = time.time()
     tree = parser.parse(script_content).pretty()
+    if profile:
+        end_time = time.time()
     CONSOLE.print(tree)
+    if profile:
+        elapsed_seconds = end_time - start_time
+        CONSOLE.print(f"Took {elapsed_seconds} seconds to parse {script.name}")
 
 
 if __name__ == "__main__":
